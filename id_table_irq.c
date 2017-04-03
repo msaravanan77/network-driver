fa5d1b3ae1cba8c76aa9500a7be57c719cb6791d
msaravanan77 <msaravanan77@gmail.com>
2017-04-03 12:13:10 +0530

    Add files via upload

--- id_table_irq.c
+++ id_table_irq.c
@@ -7,7 +7,6 @@
 #include <linux/etherdevice.h>
 
 
-
 static DEFINE_PCI_DEVICE_TABLE(my_table) = {
 	{PCI_DEVICE(0x8086, 0x100e)},
 	{}
@@ -15,14 +14,67 @@ static DEFINE_PCI_DEVICE_TABLE(my_table) = {
 
 MODULE_DEVICE_TABLE(pci, my_table);
 
+//struct e1000_adapter;
+
+static struct e1000_adapter {
+	unsigned int total_tx_bytes;
+	unsigned int total_tx_packets;
+	unsigned int total_rx_bytes;
+	unsigned int total_rx_packets;
+	//struct e1000_tx_ring *tx_ring;
+	//struct e1000_rx_ring *rx_ring;
+};
+
+
+int my_open (struct net_device *netdev)
+{
+	printk("my_open\n");
+	//printk("irq :%d\n", netdev->dev->irq);
+	int err;
+	//register irq functionalities
+	//err = request_irq(netdev->irq, );
+	return 0;
+}
+
+int my_close(struct net_device *netdev)
+{
+	printk("my_close\n");
+	return 0;
+}
+
+static netdev_tx_t my_xmit (struct sk_buff *skb, struct net_device *netdev)
+{
+	printk("start tranmit...\n");
+	return NETDEV_TX_OK;
+}
+
+static const struct net_device_ops e1000_netdev_ops = {
+	.ndo_open = my_open,
+	.ndo_stop = my_close,
+	//.ndo_start_xmit = my_xmit,
+};
+
 static int my_probe (struct pci_dev *pdev, const struct pci_device_id *id)
 {
+	int err;
 	// allocate etherdev	alloc_etherdev
-	struct net_device *ndev = alloc_etherdev(100);
+	struct net_device *netdev;
+	netdev  = alloc_etherdev(sizeof(struct e1000_adapter));
+	if (!netdev) {
+		printk("error initializing....alloc_etherdev\n");
+		return -1;
+	}
 	// Fill required structure members
+	netdev->netdev_ops = &e1000_netdev_ops;
+	
 	// register with net_device subsystem
-	printk("probe called irq :%d ", pdev->irq);
-	printk("mmio :%x\n", (unsigned int )pci_resource_start(pdev, 0));
+	err = register_netdev(netdev);
+	if (err) {
+		printk("error register_netdev...\n");
+		return -1;
+	}
+	//printk("probe called irq :%d ", pdev->irq);
+	//printk("mmio :%x\n", (unsigned int )pci_resource_start(pdev, 0));
 	return 0;
 }
 
